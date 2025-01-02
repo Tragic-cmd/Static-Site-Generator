b20b70bf7dee479e1cc61fc9f5aaf2bfa12f000c
tragic-cmd <codyshouey@gmail.com>
2025-01-01 20:36:27 -0500

    K: Updated main to recursively copy static/ to public/

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
 def main():
-    test_node = TextNode("Lets hope this works", TextType.TEXT, "https://github.com/Tragic-cmd")
-    print(test_node)
+    copy_static_to_public()
+
+def copy_static_to_public():
+    stat_path = "/home/tragic/workspace/github.com/tragic-cmd/Static-Site-Generator/static"
+    pub_path = "/home/tragic/workspace/github.com/tragic-cmd/Static-Site-Generator/public"
+
+    if os.path.exists(pub_path):
+        shutil.rmtree(pub_path)
+    os.mkdir(pub_path)
+
+    def copy_files(stat_path, pub_path):
+        for item in os.listdir(stat_path):
+            # Full path for the current item
+            full_stat_path = os.path.join(stat_path, item)
+            full_pub_path = os.path.join(pub_path, item)
+
+            # You can now work with this path to determine if it's a file or directory
+            if os.path.isfile(full_stat_path):
+                shutil.copy(full_stat_path, full_pub_path)
+            elif os.path.isdir(full_stat_path):
+                # Make directory in destination if it doesn't exist
+                if not os.path.exists(full_pub_path):
+                    os.mkdir(full_pub_path)
+                # Recursive call for directory
+                copy_files(full_stat_path, full_pub_path)  # Modify to accept two paths
+    # Start the copying process
+    copy_files(stat_path, pub_path)
 
 
 if __name__ == "__main__":
